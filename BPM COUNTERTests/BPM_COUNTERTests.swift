69f45c431188b64954fa94b8aa0cca958baa9792
ivan <ivan.n.english@gmail.com>
2026-04-25 11:23:56 -0500

    Initial Commit

---
+++ BPM COUNTERTests/BPM_COUNTERTests.swift
@@ -0,0 +1,17 @@
+//
+//  BPM_COUNTERTests.swift
+//  BPM COUNTERTests
+//
+//  Created by Ghost Buster on 4/25/26.
+//
+
+import Testing
+@testable import BPM_COUNTER
+
+struct BPM_COUNTERTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
